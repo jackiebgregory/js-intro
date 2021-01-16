dd9e14b6ee76c09b955cdd5e7664cc5fae7e8e1c
Jackie Gregory <jackiebgregory7@gmail.com>
2021-01-16 11:08:08 -0600

    added variables, data types, and math to js file

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log( this is js-intro)
+//console.log("this is js-intro");
+
+//*****START VARIABLES*****
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+//console.log(firstName + ' ' + lastName);
+//console.log(`First Name:${firstName} Last Name:${lastName}`);
+
+firstName = 'Jackie';
+lastName = 'Gregory';
+
+//console.log(firstName + ' ' + lastName);
+//console.log(`First Name:${firstName} Last Name:${lastName}`);
+
+//const favoriteFood = 'tacos';
+//console.log(favoriteFood);
+
+//favoriteFood = 'gumbo';
+//console.log(favoriteFood);
+
+//*****END VARIABLES*****
+
+//*****START DATA TYPES *****
+//const dog ='bark';
+//console.log(typeof dog);
+
+//const answer = 33;
+//const negative = -14;
+
+//console.log(typeof answer, typeof negative);
+
+//const iLoveJavascript = true;
+//console.log(typeof iLoveJavascript);
+
+//*****END DATA TYPES *****
+
+//*****START MATH *****
+//console.log(Math.pow(2,2)) // 4
+//console.log(Math.pow(3,2)); // 9
+//console.log(Math.pow(3,3)); // 27
+
+//console.log(Math.round(6.5)); // 7
+//console.log(Math.round(6.45)); // 6
+//console.log(Math.floor(6.9999)); // 6
+//console.log(Math.ceil(6.000001)); // 7
+
+const cat = 'kitty';
+console.log(cat.length); // 5
+
+//*****END MATH *****
